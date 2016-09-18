2dfbdd5bb62d4a3e663a32d6b97842ec35142c66
James Munns <james.munns@gmail.com>
2016-09-18 11:56:35 +0200

    Working build driven by xargo

--- build.rs
+++ build.rs
@@ -6,22 +6,14 @@ fn main() {
     println!("cargo:rustc-link-search=native={}", outdir);
 
     // TODO(@jamesmunns): How do I pipe stdout here so we dont just silently wait?
-    let mut cmd = Command::new("make")
+    let cmd = Command::new("make")
         .args(&["--no-print-directory", "-C", "teensy3", "NO_ARDUINO=1"])
         .output()
         .expect("failed to build Teensyduino libs");
 
-    let mut cmd2 = Command::new("arm-none-eabi-objcopy")
-        .args(&["-O",
-                "ihex",
-                "-R",
-                ".eeprom",
-                "teensy3/main.elf",
-                "teensy3.hex"])
-        .output()
-        .expect("Linker Error?");
-
-// arm-none-eabi-objcopy
+    println!("cargo:rustc-link-search={}/teensy3", env!("CARGO_MANIFEST_DIR"));
+    println!("cargo:libdir=teensy");
+    println!("cargo:rustc-link-lib=static=teensyduino");
 
     println!("It worked?");
 }
