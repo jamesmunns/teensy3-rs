dbfb92704865e01c08b4bba43add6b329dafa87a
James Munns <james.munns@gmail.com>
2016-09-26 08:07:23 +0200

    Roll back strippy clean branch

---
+++ teensy3-sys/bindings.h
@@ -0,0 +1,3 @@
+#include "teensy3/WProgram.h"
+#include "teensy3/SPI.h"
+#include "teensy3/Wire.h"
---
+++ teensy3-sys/teensy3-core/Server.h
@@ -0,0 +1,14 @@
+#if ARDUINO >= 100
+
+#ifndef server_h
+#define server_h
+
+#include "Print.h"
+
+class Server : public Print {
+public:
+  virtual void begin() =0;
+};
+
+#endif
+#endif
