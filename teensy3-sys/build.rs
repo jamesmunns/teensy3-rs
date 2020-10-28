ad2a881aec1ddc95771cf4c63d1940174e462cc8
tolvanea <alpi.tolvanen@tutanota.com>
2020-10-28 11:28:14 +0200

    Add some documentation in comments

--- teensy3-sys/build.rs
+++ teensy3-sys/build.rs
@@ -1,19 +1,31 @@
 extern crate bindgen;
 extern crate cc;
-
+//
 use std::env;
 use std::fs::{self, File};
 use std::ffi::OsStr;
 use std::io::{Read, Write};
 use std::path::PathBuf;
-//use std::process::Command;
+use std::process::Command;
 
 // Both c and c++
 static COMPILER_FLAGS: &[&str] = &[
     "-mthumb",
     "-mcpu=cortex-m4",
+    "-Os",
+    "-ffunction-sections",  // link-time garbage collection
+    "-fdata-sections",      // link-time garbage collection
+    //"-fshort-wchar",        // wide char is compiler dependent: force short one for gcc
     "-DLAYOUT_US_ENGLISH",
-    "-DUSB_SERIAL",
+    // You can enable different teensy features by changing following "-D____" flag.
+    // For example, disable keyboard/mouse functionality by replacing it with "-DUSB_SERIAL".
+    // Here's full list of possibilities from cores/teensy3/usb_dec.h (not tested!)
+    //     USB_SERIAL, USB_DUAL_SERIAL, USB_TRIPLE_SERIAL, USB_KEYBOARDONLY, USB_HID,
+    //     USB_SERIAL_HID, USB_TOUCHSCREEN, USB_HID_TOUCHSCREEN, USB_MIDI, USB_MIDI4
+    //     USB_MIDI16, USB_MIDI_SERIAL, USB_MIDI4_SERIAL, USB_MIDI16_SERIAL, USB_RAWHID
+    //     USB_FLIGHTSIM, USB_FLIGHTSIM_JOYSTICK, USB_MTPDISK, USB_AUDIO,
+    //     USB_MIDI_AUDIO_SERIAL, USB_MIDI16_AUDIO_SERIAL, USB_EVERYTHING
+    "-DUSB_SERIAL_HID",  // usb serial and hid functionality (i.e. print output & mouse/keyboard)
     "-DTEENSYDUINO",
 ];
 
@@ -36,10 +48,12 @@ struct Config {
     newlib_bits_path: PathBuf,
 }
 /// Get paths to compile
-fn get_src_paths() -> [PathBuf;1] {
+fn get_src_paths() -> [PathBuf;3] {
     // How on earth you make a globally accessible path in rust?!? Is it even possible?
     [
         PathBuf::from("cores/teensy3"),
+        PathBuf::from("SPI"),
+        PathBuf::from("Wire"),
     ]
 }
 
@@ -61,12 +75,16 @@ fn get_config() -> Config {
     } else if cfg!(feature = "teensy_3_1") || cfg!(feature = "teensy_3_2") {
         ("MK20DX256", "48000000", vec![])
     } else if cfg!(feature = "teensy_3_5") {
-        // If you want to fight errors, you can try enabling hardware floats
+        // If you want to fight linker errors, you can try enabling hardware floats
         // vec!["-mfloat-abi=hard", "-mfpu=fpv4-sp-d16"]
         ("MK64FX512", "120000000", vec![])
     } else if cfg!(feature = "teensy_3_6") {
-        // If you want to fight errors, you can try enabling hardware floats
+        // If you want to fight linker errors, you can try enabling hardware floats
         // vec!["-mfloat-abi=hard", "-mfpu=fpv4-sp-d16"]
+        // Options for -mfloat-abi, and general terminology:
+        //     soft: processor has no Floating Point Unit (FPU)
+        //     hard: processor has FPU, floating point operations are hardware instructions.
+        //     softfp: processor has FPU, but still uses soft calling convention
         ("MK66FX1M0", "180000000", vec![])
     } else {
         panic!("Uh oh");
@@ -78,13 +96,11 @@ fn get_config() -> Config {
     let first_entry = entry_iter.next().unwrap().unwrap();
     let newlib_path = first_entry.path();
 
-    let newlib_bits_path = newlib_path.join("arm-none-eabi/armv7e-m");
-    // If you want to fight errors, you can try enabling hardware floats
-    //let fpu = compiler_flags.contains(&"-mfloat-abi=hard");
-    // let newlib_bits_path = match fpu {
-    //     true  => newlib_path.join("arm-none-eabi/armv7e-m/fpu"),
-    //     false => newlib_path.join("arm-none-eabi/armv7e-m/softfp"),
-    // };
+    let has_fpu = compiler_flags.contains(&"-mfloat-abi=hard");
+    let newlib_bits_path = match has_fpu {
+        true  => newlib_path.join("arm-none-eabi/armv7e-m/fpu"),
+        false => newlib_path.join("arm-none-eabi/armv7e-m"),
+    };
 
     return Config{mcu, cpu, compiler_flags, newlib_path, newlib_bits_path}
 }
@@ -175,13 +191,13 @@ fn compile(config: &Config) {
     // Compile C Files
     c.compiler("arm-none-eabi-gcc")
         .cpp(false)
-        //.no_default_flags(true)  // Default flags seem to do something that fixes linker errrors
+        .no_default_flags(true)  // Default flags seem to do something that fixes linker errrors
         .files(c_files)
         .compile("libteensyduino_c");
     // Compile C++ Files
     cpp.compiler("arm-none-eabi-g++")
         .cpp(true)
-        //.no_default_flags(true)
+        .no_default_flags(true)
         .cpp_link_stdlib(None)
         .define("NEW_H", None) // Ignore new.h, to avoid -fno-exceptions
         .files(cpp_files)
@@ -228,13 +244,13 @@ fn generate_bindings(config: &Config) {
         .header("bindings.h")
         .generate_inline_functions(true)
         .use_core()
-        .blacklist_type("__cxxabiv1")  // something
-        .blacklist_type("__gnu_cxx")  // something
+        //.blacklist_type("__cxxabiv1")  // something
+        //.blacklist_type("__gnu_cxx")  // something
         .blacklist_item("_Tp")  // get bindings to compile
         .blacklist_item("FP_.*")  // get bindings to compile
         .blacklist_item("std")  // Ignore private items of stdlib or something
         .blacklist_item("std_.*")  // drops anything that stars with std_
-        .opaque_type("std::.*")  // Do not try to translate std types
+        .opaque_type("std::.*")  // Do not try to translate std types if any left
         .ctypes_prefix("c_types")
         .clang_args(&flags)
         .clang_args(&includes)
@@ -252,6 +268,24 @@ fn generate_bindings(config: &Config) {
 }
 
 fn main() {
+    // Testing
+    if false {  // Figure out what's inside docker container
+        let commands = [
+            "find /usr/ -name 'type_traits'",
+            "find /usr/ -name 'c++config.h'"
+        ];
+        for (i, &command) in commands.iter().enumerate() {
+            let output = Command::new("sh")
+                .arg("-c")
+                .arg(command)
+                .output()
+                .expect("Failed to execute command.");
+            let out_string = String::from_utf8_lossy(output.stdout.as_slice()) ;
+            println!("--{}--\n{}\n-----", i, out_string);
+        }
+        panic!("Stop")
+    }
+
     let config = get_config();
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
@@ -272,3 +306,6 @@ fn main() {
         .write_all(mcu.as_bytes())
         .expect("failed to write cpu info file");
 }
+// TODO what is proper way to pass libraries to bindgen?
+// TODO why --target disables visibility of some libraries?
+// TODO why defaul args for cc can not be disabled?
